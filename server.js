959d87815728da63673e94b7493b47e89be10507
wehbs <shamirwehbe@me.com>
2017-11-11 19:18:42 -0500

    3 hrs later resolved array parsing issue

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-
-require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 
 
